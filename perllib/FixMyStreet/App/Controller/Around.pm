4c827a8e423db0e9142a46c6055de336c8daf679
Fernando Uval <fernandouval@gmail.com>
2015-10-15 11:34:21 -0600

    Cambios en Around para mostrar listado

--- perllib/FixMyStreet/App/Controller/Around.pm
+++ perllib/FixMyStreet/App/Controller/Around.pm
@@ -67,6 +67,7 @@ sub around_index : Path : Args(0) {
         return $c->res->redirect($new_uri);
     }
 
+    $c->stash->{list} = 1 if $c->req->param('list');
     # Show the nearby reports
     $c->detach('display_location');
 }
