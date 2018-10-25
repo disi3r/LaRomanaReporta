ca4dccda3c048eca3a6fbcb3cb22c79eb6c14721
fernandouval <fernandouval@gmail.com>
2018-10-25 18:01:28 -0300

    Funcionalidad parcial de Service Desk

--- db/schema_0046-contacts_group_external_id_.sql
+++ db/schema_0046-contacts_group_external_id_.sql
@@ -1,9 +1,1 @@
-CREATE TABLE bodies_deadlines (
-  body_id INTEGER NOT NULL REFERENCES body(id),
-  group_id INTEGER NOT NULL REFERENCES contacts_group(group_id),
-  deadline TEXT NOT NULL,
-  max_hours INTEGER NOT NULL,
-  action TEXT,
-
-  CONSTRAINT body_group_deadline PRIMARY KEY (body_id,group_id,deadline)
-);
+ALTER TABLE contacts_group ADD COLUMN external_id TEXT;
